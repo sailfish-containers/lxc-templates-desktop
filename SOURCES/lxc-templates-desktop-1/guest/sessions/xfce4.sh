64af6fbc26fae108a0e992e12366e98ddd93950a
r3vn <giuseppe.corti@pm.me>
2020-02-13 23:41:00 +0000

    fixed wget requirements on sfos-download

--- SOURCES/lxc-templates-desktop-1/guest/sessions/xfce4.sh
+++ SOURCES/lxc-templates-desktop-1/guest/sessions/xfce4.sh
@@ -7,27 +7,34 @@ then
 	exit 0
 fi
 
-# XDG variables
-export XDG_RUNTIME_DIR=/run/user/100000
-
-# connect to qxcompositor wayland socket
-export WAYLAND_DISPLAY="../../display/wayland-container-$1"
+# unset sfos variables
+unset BROWSER
 
 # set env
+export XDG_RUNTIME_DIR=/run/user/100000
+export WAYLAND_DISPLAY="../../display/wayland-container-$1" # connect to qxcompositor wayland socket
+
 export LANG=C
-export BROWSER=chromium # replaces sailfish-browser
-#export CHROMIUM_SCALE=1.5
 export EGL_PLATFORM=wayland
 export EGL_DRIVER=egl_gallium
 export QT_QPA_PLATFORM=xcb # force qt applications backend to Xwayland
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/sbin:/sbin
+
+#export BROWSER=chromium # replaces sailfish-browser
+#export CHROMIUM_SCALE=1.5
+
+# qt applications incorrect scaling fix : break telegram desktop
+# to fix telegram start with QT_SCALE_FACTOR=1
+export QT_SCALE_FACTOR=0.1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_FONT_DPI=1400
 
 # start dbus session
 export $(dbus-launch)
 
 # Start Xwayland window
 /opt/bin/Xwayland &
-sleep 3
+sleep 2
 
 # set display to xwayland
 export DISPLAY=:0
